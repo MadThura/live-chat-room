c888cdfafdb21c8b8d2504483e08726eb0ba609d
+ <+>
2024-10-28 16:04:34 +0630

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDh1fd9evtkeYF-upcJdRWAlREJUK_J7II",
+  authDomain: "vue-blog-system-f4a80.firebaseapp.com",
+  projectId: "vue-blog-system-f4a80",
+  storageBucket: "vue-blog-system-f4a80.appspot.com",
+  messagingSenderId: "588941073990",
+  appId: "1:588941073990:web:cc390dc7e393dc4060b447",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
