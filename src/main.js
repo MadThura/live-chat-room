fcdc0d74e9cdf4ea0cf0e33b0941b7f1ee2840c2
+ <+>
2024-10-30 17:14:15 +0630

    Route Guard and wait initilize vue app for our auth system

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { auth } from "./firebase/config";
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+let app;
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App);
+    app.use(router);
+    app.mount("#app");
+  }
+});
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
+import { createRouter, createWebHistory } from "vue-router";
 import Welcome from "@/views/Welcome.vue";
 import Chatroom from "@/views/Chatroom.vue";
-import { createRouter, createWebHistory } from "vue-router";
+import { auth } from "@/firebase/config";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,14 @@ const router = createRouter({
       path: "/chatroom",
       name: "Chatroom",
       component: Chatroom,
+      beforeEnter(to, from, next) {
+        let user = auth.currentUser;
+        if (user) {
+          next();
+        } else {
+          next({ name: "Welcome" });
+        }
+      },
     },
   ],
 });
