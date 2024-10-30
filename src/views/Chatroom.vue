a2c6c150535939cfa2f79845585c8a75ee08b13a
+ <+>
2024-10-30 17:30:44 +0630

    watching the current user

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,5 +1,16 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
+import { watch } from 'vue';
+import getUser from '@/composables/getUser';
+import router from '@/router';
+
+let { user } = getUser();
+
+watch(user, () => {
+    if (!user.value) {
+        router.push("/");
+    }
+})
 </script>
 
 <template>
